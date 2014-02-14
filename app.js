096b4f9e7adc3a8a2d3d33ad796b942e9ca51897
hughbrace <hughbrace@gmail.com>
2014-02-14 16:12:44 +0000

    Created graphdb variable and returned it to http request. For testing in heroku.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var neo4j = require("neo4j");
+
 var app = express();
+var db = new neo4j.GraphDatabase("GRAPHENEDB_URL");
+
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello Manthan!');
+  res.send(db + 'Hello Manthan!');
 });
 
 var port = Number(process.env.PORT || 5000);
