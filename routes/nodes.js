3e61c9df4f1f0cceef61a32d66747c00671ef62c
hughbrace <hughbrace@gmail.com>
2014-02-18 20:38:14 +0000

    Changed database url to attempt to get cypher query running.

--- routes/nodes.js
+++ routes/nodes.js
@@ -4,7 +4,7 @@
 request = require("superagent");
 
 exports.list = function(req, res) {
-	dbURL = process.env.GRAPHENEDB_URL || "http://localhost:7474/db/data";
+	dbURL = process.env.GRAPHENEDB_URL + "db/data" || "http://localhost:7474/db/data";
 	console.log(dbURL);
 	request.post(dbURL + '/cypher').send({
 		query: 'MATCH (n) RETURN n;'
