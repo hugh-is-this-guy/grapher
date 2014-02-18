36e8162da608127c23df0fd01386e879dcfe1c67
hughbrace <hughbrace@gmail.com>
2014-02-18 20:43:45 +0000

    Connected app to neo4j database, both locally and in heroku. When url /nodes is accessed, Cypher query is run against databases retrieving all nodes, and result it returned to user.

--- routes/nodes.js
+++ routes/nodes.js
@@ -5,11 +5,9 @@ request = require("superagent");
 
 exports.list = function(req, res) {
 	dbURL = process.env.GRAPHENEDB_URL || "http://localhost:7474";
-	console.log(dbURL);
 	request.post(dbURL + '/db/data/cypher').send({
 		query: 'MATCH (n) RETURN n;'
 	}).end(function(neo4jRes) {
-		console.log(neo4jRes);
 		res.send(neo4jRes.text);
 	});
 };
