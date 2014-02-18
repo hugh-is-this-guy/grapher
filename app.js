0848cf149826ecff960966a118203df70ac7b4e5
hughbrace <hughbrace@gmail.com>
2014-02-18 16:47:14 +0000

    Backup before change to use coffee script

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var neo4j = require("neo4j");
+var express   = require("express");
+var logfmt    = require("logfmt");
+var neo4j     = require("neo4j");
 
-var app = express();
-var db = new neo4j.GraphDatabase(process.env.GRAPHENEDB_URL || "http://localhost:7474");
-
+var app   = express();
+var port = Number(process.env.PORT || 5000);
+var db    = new neo4j.GraphDatabase(process.env.GRAPHENEDB_URL || "http://localhost:7474");
 
 app.use(logfmt.requestLogger());
 
@@ -13,7 +13,6 @@ app.get('/', function(req, res) {
   res.send(db.url);
 });
 
-var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
