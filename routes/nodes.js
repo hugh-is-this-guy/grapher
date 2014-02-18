b098826e9e1344af44b397c16a3b6c9aab854984
hughbrace <hughbrace@gmail.com>
2014-02-18 20:24:28 +0000

    /nodes now returns the results from a Cypher query matching all nodes and relationships.

--- routes/nodes.js
+++ routes/nodes.js
@@ -1,7 +1,15 @@
 /*
 	GET all nodes.		
 */
+request = require("superagent");
 
 exports.list = function(req, res) {
-	res.send("all my nodes!");
+	dbURL = process.env.GRAPHENEDB_URL || "http://localhost:7474/db/data";
+	console.log(dbURL);
+	request.post(dbURL + '/cypher').send({
+		query: 'MATCH (n) RETURN n;'
+	}).end(function(neo4jRes) {
+		console.log(neo4jRes.text);
+		res.send(neo4jRes.text);
+	});
 };
