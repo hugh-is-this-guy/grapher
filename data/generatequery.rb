46ccaeb20eace144fc3908c7336ee2588554f645
hughbrace <hughbrace@gmail.com>
2014-02-24 21:10:09 +0000

    Cypher query generation fil renamed to generatquery.rb. It now outputs results to a file called query.

---
+++ data/generatequery.rb
@@ -0,0 +1,36 @@
+names = Array.new
+
+def process_lines(filename)
+  count = 0
+  File.open(filename, 'r') do |f|
+    f.each_line do |line|
+      yield line.strip, count
+      count += 1
+    end
+  end
+end
+
+process_lines("forenames") { |line, count| 14.times { |i| names.push line }}
+
+process_lines("surnames")  do |line, count| 
+  14.times do |i|
+    names[(150 * i ) + count] += "#{line}" 
+  end
+end
+
+File.open("query", 'w') { |output|
+  for i in 1..1899 do
+    output.puts "CREATE (#{names[i]}:Person {name: '#{names[i]}'});"
+  end
+ 
+  process_lines("relationships") { |line, count| 
+    from, to, weight = line.split
+    from = names[Integer(from)]
+    to = names[Integer(to)]
+    output.puts "MATCH (#{from}:Person), (#{to}:Person)
+    WHERE #{from}.name = '#{from}' AND #{to}.name = '#{to}'
+    CREATE (#{from}) - [r:Knows{weight: #{weight}}] -> (#{to});"
+  }
+}
+
+
